065c7c15e289e1ba8c95492708b58188151d69d0
DESKTOP-9NUUM9N\m <ihindawi@gmail.com>
2019-01-02 22:34:48 +0200

    Update main.cpp

--- DarkFortress/src/main.cpp
+++ DarkFortress/src/main.cpp
@@ -6,23 +6,6 @@
 int main ( )
 {
 
-	//Weapon Sword ( 23 );
-
-	//Weapon Spear ( 27 );
-
-	//switch (int x = 0)
-	//{
-	//case (1):
-	//	{
-	//		break;
-	//	}
-	//default:
-
-	//}
-
-	//Hero h ( Sword.CalculateDamage ( ) );
-	//Weapon Sword ( "s", 23 );
-
 	WeaponManager wm;
 	Weapon Spear = wm.GetSpear ( );
 
